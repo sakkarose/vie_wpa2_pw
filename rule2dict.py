f67b6d44c155f3097d6c81cbecdb6d99d441f83a
Dau Trong Hoang <ame@mizu.reisen>
2025-01-27 11:13:39 +0700

    Update rule2dict.py

--- rule2dict.py
+++ rule2dict.py
@@ -1,19 +1,15 @@
 import itertools
 
-def generate_wordlist(rule_file):
-    wordlist =
-    with open(rule_file, 'r') as f:
+def generate_wordlist(rule_file, output_file):
+    with open(rule_file, 'r') as f, open(output_file, 'w') as outfile:
         for line in f:
             line = line.strip()
             parts = line.split('?d')
-            # Generate all possible combinations of digits for the ?d placeholders
             for digits in itertools.product('0123456789', repeat=line.count('?d')):
                 word = ''.join([parts[i] + digits[i] for i in range(len(parts) - 1)]) + parts[-1]
-                wordlist.append(word)
-    return wordlist
+                outfile.write(word + '\n')
 
 if __name__ == '__main__':
-    rule_file = 'your_rule_file.rule'
-    wordlist = generate_wordlist(rule_file)
-    for word in wordlist:
-        print(word)
+    rule_file = 'test.rule'
+    output_file = 'test.txt'
+    generate_wordlist(rule_file, output_file)
